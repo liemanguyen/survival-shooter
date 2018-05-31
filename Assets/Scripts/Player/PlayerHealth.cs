21116c9bd0cca2265a68e6e670cf30574e4b1e04
Liem Nguyen <liemnguyen974@gmail.com>
2018-05-30 23:57:36 -0700

    harming enemies

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+
     public int startingHealth = 100;
     public int currentHealth;
     public Slider healthSlider;
@@ -14,24 +15,21 @@ public class PlayerHealth : MonoBehaviour
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
-
     Animator anim;
     AudioSource playerAudio;
     PlayerMovement playerMovement;
-    //PlayerShooting playerShooting;
+    PlayerShooting playerShooting;
     bool isDead;
     bool damaged;
-
 
     void Awake ()
     {
         anim = GetComponent <Animator> ();
         playerAudio = GetComponent <AudioSource> ();
         playerMovement = GetComponent <PlayerMovement> ();
-        //playerShooting = GetComponentInChildren <PlayerShooting> ();
+        playerShooting = GetComponentInChildren <PlayerShooting> ();
         currentHealth = startingHealth;
     }
-
 
     void Update ()
     {
@@ -46,39 +44,28 @@ public class PlayerHealth : MonoBehaviour
         damaged = false;
     }
 
-
     public void TakeDamage (int amount)
     {
         damaged = true;
-
         currentHealth -= amount;
-
         healthSlider.value = currentHealth;
-
         playerAudio.Play ();
-
         if(currentHealth <= 0 && !isDead)
         {
             Death ();
         }
     }
 
-
     void Death ()
     {
         isDead = true;
-
-        //playerShooting.DisableEffects ();
-
+        playerShooting.DisableEffects ();
         anim.SetTrigger ("Die");
-
         playerAudio.clip = deathClip;
         playerAudio.Play ();
-
         playerMovement.enabled = false;
-        //playerShooting.enabled = false;
+        playerShooting.enabled = false;
     }
-
 
     public void RestartLevel ()
     {
