e67252e3385b498a7c1e276fe0993b5a682890e7
Liem Nguyen <liemnguyen974@gmail.com>
2018-05-31 21:15:10 -0700

    scoring points

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -2,12 +2,12 @@
 
 public class EnemyHealth : MonoBehaviour
 {
+
     public int startingHealth = 100;
     public int currentHealth;
     public float sinkSpeed = 2.5f;
     public int scoreValue = 10;
     public AudioClip deathClip;
-
 
     Animator anim;
     AudioSource enemyAudio;
@@ -16,17 +16,14 @@ public class EnemyHealth : MonoBehaviour
     bool isDead;
     bool isSinking;
 
-
     void Awake ()
     {
         anim = GetComponent <Animator> ();
         enemyAudio = GetComponent <AudioSource> ();
         hitParticles = GetComponentInChildren <ParticleSystem> ();
         capsuleCollider = GetComponent <CapsuleCollider> ();
-
         currentHealth = startingHealth;
     }
-
 
     void Update ()
     {
@@ -36,45 +33,35 @@ public class EnemyHealth : MonoBehaviour
         }
     }
 
-
     public void TakeDamage (int amount, Vector3 hitPoint)
     {
         if(isDead)
             return;
-
         enemyAudio.Play ();
-
         currentHealth -= amount;
-            
         hitParticles.transform.position = hitPoint;
         hitParticles.Play();
-
         if(currentHealth <= 0)
         {
             Death ();
         }
     }
 
-
     void Death ()
     {
         isDead = true;
-
         capsuleCollider.isTrigger = true;
-
         anim.SetTrigger ("Dead");
-
         enemyAudio.clip = deathClip;
         enemyAudio.Play ();
     }
-
 
     public void StartSinking ()
     {
         GetComponent <UnityEngine.AI.NavMeshAgent> ().enabled = false;
         GetComponent <Rigidbody> ().isKinematic = true;
         isSinking = true;
-        //ScoreManager.score += scoreValue;
+        ScoreManager.score += scoreValue;
         Destroy (gameObject, 2f);
     }
 }
